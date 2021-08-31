006e50524ee4123a576e76bbe69f5104baf34d2a
Maipal Reddy <maipalr@gmail.com>
2021-08-31 09:41:28 +0530

    page size count changes

--- app/src/main/java/com/example/sampleapplication/ImageList/Util/Constants.kt
+++ app/src/main/java/com/example/sampleapplication/ImageList/Util/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
         const val BASE_URL = "flickr.photos.getRecent" //"flickr.photos.search"
         const val API_KEY = "062a6c0c49e4de1d78497d13a7dbb360"
         const val SEARCH_DELAY_INTERVAL = 500L
-        const val QUERY_PAGE_SIZE = 5
+        const val QUERY_PAGE_SIZE = 10
     }
 }
